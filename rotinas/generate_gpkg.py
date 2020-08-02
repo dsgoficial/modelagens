d74175c9dbcaa362ee5b1ef7fa65f95674f1a712
Eliton <elitonfilho@gmail.com>
2020-08-02 19:52:35 -0300

    Changes on GPKG generation

--- rotinas/generate_gpkg.py
+++ rotinas/generate_gpkg.py
@@ -10,6 +10,17 @@ SQL = Path(__file__, '../../edgv_300/edgv_300.sql').resolve()
 MASTERFILE = Path(__file__, '../../edgv_300/master_file_300.json').resolve()
 output = Path.cwd() / 'teste.gpkg'
 
+# Options
+gdal.SetConfigOption('CREATE_METADATA_TABLES', 'NO')
+dataset_options = ['VERSION=1.2', 'ADD_GPKG_OGR_CONTENTS=YES']
+layer_options = ['SPATIAL_INDEX=YES']
+
+with open(MASTERFILE, 'r') as f:
+    mf = json.load(f)
+
+srs = osr.SpatialReference()
+srs.ImportFromEPSG(mf['coord_sys'])
+
 
 class SqlParser():
 
@@ -19,10 +30,11 @@ class SqlParser():
 
     def getCreateTables(self):
         return re.findall(r'(?s)CREATE TABLE edgv\.([A-Za-z_]+)\(\n(.+?)(?=\n\);)', self.sql)
-        
+
     def parseFields(self, tableIter):
         fields = []
-        keys = tableIter.strip(' ').replace('\n', '').replace('\t', '').split(',')
+        keys = tableIter.strip(' ').replace(
+            '\n', '').replace('\t', '').split(',')
         for key in keys:
             splits = key.split(' ')
             if len(splits) <= 2:
@@ -52,46 +64,71 @@ class SqlParser():
         elif suffix == 'p':
             return ogr.wkbMultiPoint
 
-# Options
-gdal.SetConfigOption('CREATE_METADATA_TABLES', 'NO')
-dataset_options = ['VERSION=1.2', 'ADD_GPKG_OGR_CONTENTS=YES']
-layer_options = ['SPATIAL_INDEX=YES']
+def getGeomType(geom_type):
+    mapGeomType = {
+        'MultiPoint': ogr.wkbMultiPoint,
+        'MultiLinestring': ogr.wkbMultiLineString,
+        'MultiPolygon': ogr.wkbMultiPolygon
+    }
+    return mapGeomType[geom_type]
+
+def getAttrDataType(x):
+    mapDataType = {
+        'boolean': (ogr.OFTInteger, ogr.OFSTBoolean),
+        'timestamp': (ogr.OFTDateTime, ogr.OFSTNone),
+        'smallint': (ogr.OFTInteger, ogr.OFSTInt16),
+        'integer': (ogr.OFTInteger, ogr.OFSTNone),
+        'real': (ogr.OFTReal, ogr.OFSTNone)
+    }
+    # Needs to return varchar with size
+    if str(x).lower().startswith('varchar'):
+        length = re.search(r'varchar\((.+)\)', x)
+        return (ogr.OFTString, ogr.OFSTNone ,length.group(1))
+    else:
+        return mapDataType[x]
 
 
 parser = SqlParser(SQL)
-with open(MASTERFILE, 'r') as f:
-    mf = json.load(f)
-
-srs = osr.SpatialReference()
-srs.ImportFromEPSG(mf['coord_sys'])
 
 # Organizing masterfile to get domain restrictions
 full_codelist = []
 for classe in mf['classes']:
-    attr_in_class = []
-    for attr in classe['atributos']:
-        if attr.get('mapa_valor'):
-            if attr.get('valores'):
-                value_map = [x['code'] for x in attr['valores']]
+    for prim in classe['primitivas']:
+        attr_in_class = []
+        for attr in classe['atributos']:
+            if attr.get('mapa_valor'):
+                if attr.get('valores'):
+                    value_map = [x['code'] for x in attr['valores']]
+                else:
+                    domain = [x for x in mf['dominios']
+                                if x['nome'] == attr['mapa_valor']][0]
+                    value_map = [x['code'] for x in domain['valores']]
+                valueSet = (attr['nome'],getAttrDataType(attr['tipo']), value_map)
+                attr_in_class.append(valueSet)
             else:
-                domain = [x for x in mf['dominios'] if x['nome'] == attr['mapa_valor']][0]
-                value_map = [x['code'] for x in domain['valores']]
-            valueSet = (attr['nome'], value_map)
-            attr_in_class.append(valueSet)
-    classSet = (classe['nome'], attr_in_class)
-    full_codelist.append(classSet)
+                valueSet = (attr['nome'], getAttrDataType(attr['tipo']))
+                attr_in_class.append(valueSet)
+        classSet = (
+            f"{mf['schema_dados']}_{classe['nome']}{mf['geom_suffix'][prim]}",
+            getGeomType(prim),
+            attr_in_class)
+        full_codelist.append(classSet)
 
 # ds = ogr.GetDriverByName('GPKG').CreateDataSource(output, options = dataset_options)
 
 ds = gdal.GetDriverByName('GPKG').Create(str(output), 0,0,0,0)
-# Insert classes
-for item in parser.getCreateTables():
-    lyr = ds.CreateLayer(item[0], geom_type = parser.getGeomType(item[0]), options = layer_options, srs = srs)
-    for field in parser.parseFields(item[1]):
+# print(full_codelist)
+
+for item in full_codelist:
+    lyr = ds.CreateLayer(item[0], geom_type = item[1], options = layer_options, srs = srs)
+    for field in item[2]:
         defn = ogr.FieldDefn(field[0])
-        defn.SetType(field[1])
-        if defn.GetTypeName() == 'Integer':
-            defn.SetSubType(ogr.OFSTInt16)
+        typ, styp, *extra = field[1]
+        defn.SetType(typ)
+        if styp:
+            defn.SetSubType(styp)
+        if extra and typ == ogr.OFTString:
+            defn.SetWidth(int(extra[0]))
         lyr.CreateField(defn)
 
 # # Insert domain restrictions
